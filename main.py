b8172fd4938ed8c283d5b7d311eb2ddf4dfae066
TimKapf <kapferer.tim@icloud.com>
2022-07-28 22:00:21 +0200

    Answers, DB integration (not ideal)

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, PollAnswerHandler, PollHandler
 import re
+import math 
+
+import sqlite3 as sl
 
 #TODO Prevent bots from sending messages by checking update effetive user is_bot
 
 load_dotenv()
 TOKEN = os.environ.get('TELEGRAM_API_KEY')
 GROUP_ID = os.environ.get('GROUP_ID')
+
+DB_NAME = os.environ.get("DB_NAME")
+con = sl.connect(DB_NAME)
+cur = con.cursor()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -41,15 +48,21 @@ async def pw_auth(update:Update, context: ContextTypes.DEFAULT_TYPE):
         return
         
     #TODO Change into usefull helptext
-    await user_auth_pw(chat_id=chat_id, context=context, password=' '.join(context.args))
+    await user_auth_pw(chat_id=chat_id, context=context, user_id=update.effective_user.id,password=' '.join(context.args))
     
 async def ask_q(update:Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     if chat_id == int(GROUP_ID):
         return
     
-    #TODO Create Q in DB with ID and User ID and Chat ID
-    await context.bot.send_message(chat_id=GROUP_ID, text=(' '.join(context.args) + "\n\nID:1337"))
+    cur.execute("SELECT id FROM USER WHERE telegram_id=?", (update.effective_user.id,))
+    user_id = cur.fetchone()[0]
+
+    cur.execute(
+        "INSERT INTO QUESTIONS (user_id, q_text) VALUES (?,?)", (user_id, ' '.join(context.args))
+        )
+    id = cur.lastrowid
+    await context.bot.send_message(chat_id=GROUP_ID, text=(' '.join(context.args) + "\n\nID: " + str(id)))
 
 async def create_poll(update:Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
@@ -66,32 +79,143 @@ async def create_poll(update:Update, context: ContextTypes.DEFAULT_TYPE):
         allows_multiple_answers=poll.allows_multiple_answers
         )
 
-async def answer_q(update:Update, context: ContextTypes.DEFAULT_TYPE):
+async def answer_q_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     if chat_id == int(GROUP_ID):
         return
-    #TODO Create A in DB with id and if private or open
-    #TODO Connect A to Q in DB
-    #TODO Fetch user ID of asked Q
-    #TODO Send Answer to user who asked Q 
-    #TODO Set if answer is open or private
-    await context.bot.send_message(chat_id=chat_id, text="HELPTEXT")
-
-
     
+    a_text = ' '.join(context.args)
+    user_id = update.effective_user.id
+    anon = True
+    in_group = False
+
+    q_id = context.args[0]
+    
+    if not q_id.isnumeric():
+        await context.bot.send_message(chat_id=chat_id, text="Please enter first the ID-Number and then your reply. Try again!")
+        return 
+    
+    cur.execute("SELECT id FROM QUESTIONS WHERE id=?", (q_id,))
+    entry = cur.fetchall()
+    if len(entry) == 0:
+        await context.bot.send_message(chat_id=chat_id, text="It seems that the ID-Number is incorrect. Try again!")
+        return 
+
+    cur.execute(
+        "INSERT INTO ANSWERS (q_id, user_id, a_text, anon, in_group) VALUES (?,?,?,?,?)", 
+        (q_id, user_id, 'ID: ' + a_text, anon, in_group)
+        )   
+
+    cur.execute("SELECT user_id FROM QUESTIONS WHERE id=?", (q_id,))
+    user_id = cur.fetchone()
+
+    cur.execute("SELECT telegram_id FROM USER WHERE id=?", user_id)
+    chat_id = cur.fetchone()[0]
+    
+    await context.bot.send_message(chat_id=int(chat_id), text="ID: " + a_text)
+
+async def answer_q_group_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
+
+    chat_id = update.effective_chat.id
+    if chat_id == int(GROUP_ID):
+        return
+    
+    a_text = ' '.join(context.args)
+    user_id = update.effective_user.id
+    anon = True
+    in_group = True
+
+    q_id = context.args[0]
+    
+    if not q_id.isnumeric():
+        await context.bot.send_message(chat_id=chat_id, text="Please enter first the ID-Number and then your reply. Try again!")
+        return 
+    
+    cur.execute("SELECT id FROM QUESTIONS WHERE id=?", (q_id,))
+    entry = cur.fetchall()
+    if len(entry) == 0:
+        await context.bot.send_message(chat_id=chat_id, text="It seems that the ID-Number is incorrect. Try again!")
+        return 
+
+    cur.execute(
+        "INSERT INTO ANSWERS (q_id, user_id, a_text, anon, in_group) VALUES (?,?,?,?,?)", 
+        (q_id, user_id, 'ID: ' + a_text, anon, in_group)
+        )   
+
+    await context.bot.send_message(chat_id=int(GROUP_ID), text="ID: " + a_text)
+
+
+async def report_question(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+
+    if not context.args[0].isnumeric():
+        await context.bot.send_message(chat_id=chat_id, text="Enter the ID-Number first, followed by your reason!")
+        return
+
+    reporter_id = update.effective_user.id
+    cur.execute("SELECT user_id FROM QUESTIONS WHERE id=?", (context.args[0],))
+    reported_user_id = cur.fetchone()[0]
+    
+    cur.execute(
+        "INSERT INTO  REPORTS (reporter_id, reported_user_id, reported_q_id, why) VALUES (?,?,?,?)", 
+        (reporter_id, reported_user_id, context.args[0], ' '.join(context.args[1:]))
+        )   
+
+async def report_answer(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+
+    if not context.args[0].isnumeric():
+        await context.bot.send_message(chat_id=chat_id, text="Enter the ID-Number first, followed by your reason!")
+        return
+
+    reporter_id = update.effective_user.id
+    cur.execute("SELECT user_id FROM ANSWERS WHERE id=?", (context.args[0],))
+    reported_user_id = cur.fetchone()[0]
+    
+    cur.execute(
+        "INSERT INTO  REPORTS (reporter_id, reported_user_id, reported_a_id, why) VALUES (?,?,?,?)", 
+        (reporter_id, reported_user_id, context.args[0], ' '.join(context.args[1:]))
+        )   
+
 #HELPERS
 async def user_auth(chat_id, user_id, context):
     if context.bot.get_chat_member(chat_id, user_id):
         #TODO Set auth in DB
+
+        cur.execute("SELECT telegram_id FROM USER WHERE telegram_id=?", (user_id,))
+        entry = cur.fetchall()
+        if len(entry) == 0:
+            # Create Data Entry
+            with con:
+                con.execute(
+                "INSERT INTO USER (telegram_id) VALUES (?)", (user_id,)
+                )
+        else:
+            await context.bot.send_message(chat_id=chat_id, text="You are already authorised!")
+            return True
+
         await context.bot.send_message(chat_id=chat_id, text="Congrats you can now use the bot! Enter the /help command to see how everything works")
         return True
     else:
         await context.bot.send_message(chat_id=chat_id, text="Seems like you are not in the group or there was a mistake while trying to authenticate you, pls try /start again or enter the command /password followed by the password for our special website")
         return False
     
-async def user_auth_pw(chat_id, context, password):
+async def user_auth_pw(chat_id, user_id, context, password):
     if password == os.environ.get("AUTH"):
         #TODO Set auth in DB
+
+        # TODO Repetitive 
+        cur.execute("SELECT telegram_id FROM USER WHERE telegram_id=?", (user_id,))
+        entry = cur.fetchall()
+        if len(entry) == 0:
+            # Create Data Entry
+            with con:
+                con.execute(
+                "INSERT INTO USER (telegram_id) VALUES (?)", (user_id,)
+                )
+        else:
+            await context.bot.send_message(chat_id=chat_id, text="You are already authorised!")
+            return True
         
         await context.bot.send_message(chat_id=chat_id, text="Congrats you can now use the bot! Enter the /help command to see how everything works")
         asyncio.run(help_cmd)
@@ -112,10 +236,15 @@ if __name__ == "__main__":
     handlers.append(CommandHandler('help', help_cmd))
     handlers.append(CommandHandler('password', pw_auth))
     handlers.append(CommandHandler('q', ask_q))
+    #handlers.append(CommandHandler('report', report))
     handlers.append(MessageHandler(filters.POLL, create_poll))
+
+    #handlers.append(MessageHandler(filters.REPLY), answer_q_reply)
+    handlers.append(CommandHandler('a', answer_q_command))
+    handlers.append(CommandHandler('a_group', answer_q_group_command))
     
     application.add_handlers(handlers=handlers)
     
     application.run_polling()
-    
+
     
